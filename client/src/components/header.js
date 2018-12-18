414601e2b4204f735e34dc3758e69db5594c1bb3
m.ilieva <magdalena.e.ilieva@gmail.com>
2018-12-18 17:17:39 +0200

    header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,13 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/contact'>contact</Link></li>
-            </ul>
-        </nav>
+
+    <header className="navbar navbar-inverse navbar-fixed-top " role="banner">
+        <div className="container">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span className="sr-only">Toggle navigation</span>
+                    <i className="fa fa-bars"></i>
+                </button>
+                <a className="navbar-brand"><h1><span
+                    className="pe-7s-gleam bounce-in"></span> IMPACT</h1></a>
+            </div>
+            <nav className="collapse navbar-collapse">
+                <ul className="nav navbar-nav navbar-right">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/contact'>contact</Link></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 );
 
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Home extends Component {
 
-  render() {
-    return (
-      <div id="home" className="main">
-        {/*<NavigationBar />*/}
-        This is Home!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="home" className="main">
+                <section id="main-slider" className="no-margin">
+                    <div className="carousel slide" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="item active item active">
+                                <div className="container">
+                                    <div className="col-sm-12">
+                                        <div className="carousel-content center centered">
+                                            <span className="home-icon pe-7s-gleam bounce-in"></span>
+                                            <h2 className="boxed animation animated-item-1 fade-down">WE GONNA HELP YOU
+                                                MAKE AN IMPACT</h2>
+                                            <p className="boxed animation animated-item-2 fade-up">Our expertise will
+                                                guide you to success. Without Fail.</p>
+                                            <br/>
+                                            <a className="btn btn-md animation bounce-in" href="#services">Learn
+                                                More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 

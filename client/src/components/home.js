2bf5dc3304ded6f97d82ebba3361e5584177e168
m.ilieva <magdalena.e.ilieva@gmail.com>
2018-12-18 19:11:02 +0200

    CONTENT

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -8,15 +8,17 @@ class Home extends Component {
                 <section id="main-slider" className="no-margin">
                     <div className="carousel slide" data-ride="carousel">
                         <div className="carousel-inner">
+                            <img src="/lavender-back.jpg" className="back-image" role="presentation"/>
                             <div className="item active item active">
                                 <div className="container">
                                     <div className="col-sm-12">
                                         <div className="carousel-content center centered">
                                             <span className="home-icon pe-7s-gleam bounce-in"></span>
-                                            <h2 className="boxed animation animated-item-1 fade-down">WE GONNA HELP YOU
-                                                MAKE AN IMPACT</h2>
-                                            <p className="boxed animation animated-item-2 fade-up">Our expertise will
-                                                guide you to success. Without Fail.</p>
+                                            <h2 className="boxed animation animated-item-1 fade-down">LAVENDER
+                                                PRODUCTION - BULGARIA</h2>
+                                            <br/>
+                                            <p className="boxed animation animated-item-2 fade-up">The Life of quality
+                                                and prestige</p>
                                             <br/>
                                             <a className="btn btn-md animation bounce-in" href="#services">Learn
                                                 More</a>
@@ -27,6 +29,172 @@ class Home extends Component {
                         </div>
                     </div>
                 </section>
+                <div id="content-wrapper">
+                    <section id="about-us" className="white">
+                        <div className="container">
+                            <div className="gap"></div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="center gap fade-down section-heading">
+                                        <h2 className="main-title">A Little About Us</h2>
+                                        <hr/>
+                                        <p>Of an or game gate west face shed. ﻿no great but music too old found
+                                            arose.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-10 col-md-offset-1 fade-up">
+                                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis
+                                        bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis
+                                        sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                                        velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris
+                                        vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora
+                                        torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo.
+                                        Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque
+                                        elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra,
+                                        erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl
+                                        quis neque. Suspendisse in orci enim.</p>
+
+                                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis
+                                        bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis
+                                        sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                                        velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris
+                                        vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora
+                                        torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo.
+                                        Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque
+                                        elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra,
+                                        erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl
+                                        quis neque. Suspendisse in orci enim.</p>
+                                </div>
+                                <div className="col-md-4 fade-up">
+
+                                </div>
+                            </div>
+                            <div className="gap"></div>
+                            <div className="center gap fade-down section-heading">
+                                <h2 className="main-title">Meet The Team</h2>
+                                <hr/>
+                                <p>Of an or game gate west face shed. ﻿no great but music too old found arose.</p>
+                            </div>
+                            <div className="gap"></div>
+
+                            <div id="meet-the-team" className="row">
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team01.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team01.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
+                                        </div>
+                                        <div className="team-content fade-up">
+                                            <h5>Daniel Jones
+                                                <small className="role muted">Web Design</small>
+                                            </h5>
+                                            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+                                            <a className="btn btn-social btn-facebook" href="#"><i
+                                                className="fa fa-facebook"></i></a> <a
+                                            className="btn btn-social btn-google-plus" href="#"><i
+                                            className="fa fa-google-plus"></i></a> <a
+                                            className="btn btn-social btn-twitter" href="#"><i
+                                            className="fa fa-twitter"></i></a> <a
+                                            className="btn btn-social btn-linkedin" href="#"><i
+                                            className="fa fa-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team02.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team02.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
+                                        </div>
+                                        <div className="team-content fade-up">
+                                            <h5>John Smith
+                                                <small className="role muted">Marketing Director</small>
+                                            </h5>
+                                            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+                                            <a className="btn btn-social btn-facebook" href="#"><i
+                                                className="fa fa-facebook"></i></a> <a
+                                            className="btn btn-social btn-google-plus" href="#"><i
+                                            className="fa fa-google-plus"></i></a> <a
+                                            className="btn btn-social btn-twitter" href="#"><i
+                                            className="fa fa-twitter"></i></a> <a
+                                            className="btn btn-social btn-linkedin" href="#"><i
+                                            className="fa fa-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team03.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team03.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
+                                        </div>
+                                        <div className="team-content fade-up">
+                                            <h5>Dave Gorman
+                                                <small className="role muted">Web Design</small>
+                                            </h5>
+                                            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+                                            <a className="btn btn-social btn-facebook" href="#"><i
+                                                className="fa fa-facebook"></i></a> <a
+                                            className="btn btn-social btn-google-plus" href="#"><i
+                                            className="fa fa-google-plus"></i></a> <a
+                                            className="btn btn-social btn-twitter" href="#"><i
+                                            className="fa fa-twitter"></i></a> <a
+                                            className="btn btn-social btn-linkedin" href="#"><i
+                                            className="fa fa-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team04.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team04.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
+                                        </div>
+                                        <div className="team-content fade-up">
+                                            <h5>Steve Smith
+                                                <small className="role muted">Sales Assistant</small>
+                                            </h5>
+                                            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+                                            <a className="btn btn-social btn-facebook" href="#"><i
+                                                className="fa fa-facebook"></i></a> <a
+                                            className="btn btn-social btn-google-plus" href="#"><i
+                                            className="fa fa-google-plus"></i></a> <a
+                                            className="btn btn-social btn-twitter" href="#"><i
+                                            className="fa fa-twitter"></i></a> <a
+                                            className="btn btn-social btn-linkedin" href="#"><i
+                                            className="fa fa-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="gap"></div>
+                            <div className="gap"></div>
+                        </div>
+                    </section>
+                </div>
             </div>
         );
     }

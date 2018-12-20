3726447d9ae3f2ab472f5cce76bef78c46c1d703
m.ilieva <magdalena.e.ilieva@gmail.com>
2018-12-20 19:44:22 +0200

    test

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -10,11 +10,10 @@ class Home extends Component {
                         <div className="carousel-inner">
                             <img src="/lavender-back.jpg" className="back-image" role="presentation"/>
                             <div className="item active item active">
-                                <div className="container">
+                                <div className="container main-container">
                                     <div className="col-sm-12">
-                                        <div className="carousel-content center centered">
-                                            <span className="home-icon pe-7s-gleam bounce-in"></span>
-                                            <h2 className="boxed animation animated-item-1 fade-down">LAVENDER OIL -
+                                        <div className="carousel-content centered">
+                                            <h2 className="boxed animation animated-item-1 fade-down header-title">LAVENDER OIL -
                                                 PRODUCTION</h2>
                                             <br/>
                                             <h3 className="boxed animation animated-item-2 fade-up"> The Gold Of
@@ -38,7 +37,7 @@ class Home extends Component {
                             <div className="gap"></div>
                             <div className="row">
                                 <div className="col-md-12">
-                                    <div className="center gap fade-down section-heading">
+                                    <div className=" gap fade-down section-heading">
                                         <h2 className="main-title">A Little About Us</h2>
                                         <hr/>
                                         <div className="testimonial-quote">
@@ -74,62 +73,48 @@ class Home extends Component {
                     <section id="portfolio" className="white">
                         <div className="container">
                             <div className="gap"></div>
-                            <div className="center gap fade-down section-heading">
+                            <div className=" gap fade-down section-heading">
                                 <h2 className="main-title">The Process</h2>
                                 <hr/>
                                 <p>Great ideas always have a beginning</p>
                             </div>
                             <div className="row">
-                                <div className="col-md-3 col-xs-6">
-                                    <div className="center team-member">
-                                        <div className="team-image">
-                                            <img className="img-responsive img-thumbnail bounce-in"
-                                                 src="/production-11.jpg" alt=""/>
-                                        </div>
-                                        <div className="team-content fade-up">
-                                            <h5>
-                                                Precisely choosing the best sorts and sow the seeds in the
-                                                ground.
+                                <div className="col-md-6 col-xs-12">
+                                    <div className="production-img"
+                                         style={{backgroundImage: "url(/production-11.jpg"}}/>
+                                    <div className="team-content fade-up">
+                                        <h5>
+                                            Best sowed seeds
 
-                                            </h5>
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-xs-12">
+                                    <div className="production-img"
+                                         style={{backgroundImage: "url(/production-22.jpg)"}}/>
+                                    <div className="team-content fade-up">
+                                        <h5>
+                                           Manual work guaranties quality
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-xs-12">
+                                    <div className="team-member">
+                                        <div className="production-img"
+                                             style={{backgroundImage: "url(/production-33.jpeg)"}}/>
 
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-3 col-xs-6">
-                                    <div className="center team-member">
-                                        <div className="team-image">
-                                            <img className="img-responsive img-thumbnail bounce-in"
-                                                 src="/production-22.jpg" alt=""/>
-                                        </div>
                                         <div className="team-content fade-up">
                                             <h5>
-                                                Tracking the whole process of the lavender growth and removing
-                                                weeds by hand if necessary.
+                                               Oil extraction under strictly controlled conditions
                                             </h5>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-md-3 col-xs-6">
-                                    <div className="center team-member">
-                                        <div className="team-image">
-                                            <img className="img-responsive img-thumbnail bounce-in"
-                                                 src="/production-33.jpeg" alt=""/>
-                                        </div>
-                                        <div className="team-content fade-up">
-                                            <h5>
-                                                Gather the harvest and transport it to the closest distillery in
-                                                order to extract the oil
-                                            </h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-3 col-xs-6">
-                                    <div className="center team-member">
-                                        <div className="team-image">
-                                            <img className="img-responsive img-thumbnail bounce-in"
-                                                 src="/production-44.jpg" alt=""/>
-                                        </div>
+                                <div className="col-md-6 col-xs-12">
+                                    <div className="team-member">
+                                        <div className="production-img"
+                                             style={{backgroundImage: "url(/production-44.jpg)"}}/>
+
                                         <div className="team-content fade-up">
                                             <h5>Lavender oil is stored and ready to use. </h5>
                                         </div>
@@ -139,7 +124,7 @@ class Home extends Component {
                         </div>
                     </section>
                     <div className="container">
-                        <div className="center gap fade-down section-heading">
+                        <div className="gap fade-down section-heading">
                             <h2 className="main-title">What We Can Provide</h2>
                         </div>
                     </div>
@@ -157,7 +142,7 @@ class Home extends Component {
                                 <div className="col-md-3 col-xs-6">
                                     <div className="center bounce-in">
                                         <span className="stat-icon"><i className="far fa-file-alt"/></span>
-                                        <h3>ASSISTANTS OF TRANSPORTATION AND DOCUMENTS</h3>
+                                        <h3>TRANSPORTATION DOCUMENTS</h3>
                                     </div>
                                 </div>
                                 <div className="col-md-3 col-xs-6">
@@ -178,58 +163,60 @@ class Home extends Component {
                     </section>
 
                     <section>
-                        <div className="gap"></div>
-                        <div className="center gap fade-down section-heading">
-                            <h2 className="main-title">Meet The Team</h2>
-                            <hr/>
-                            <p>Of an or game gate west face shed. ﻿no great but music too old found arose.</p>
-                        </div>
-                        <div id="meet-the-team" className="row">
-                            <div className="col-md-3 col-xs-6">
-                                <div className="center team-member">
-                                    <div className="team-image">
-                                        <img className="img-responsive img-thumbnail bounce-in"
-                                             src="images/team/team01.jpg" alt=""/>
-                                        <div className="overlay">
-                                            <a className="preview btn btn-outlined btn-primary"
-                                               href="images/team/team01.jpg" rel="prettyPhoto"><i
-                                                className="fa fa-eye"></i></a>
+                        <div className="container">
+                            <div className="gap"></div>
+                            <div className="gap fade-down section-heading">
+                                <h2 className="main-title">Meet Our Family</h2>
+                                <hr/>
+                                <p>Of an or game gate west face shed. ﻿no great but music too old found arose.</p>
+                            </div>
+                            <div id="meet-the-team" className="row">
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team01.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team01.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="col-md-3 col-xs-6">
-                                <div className="center team-member">
-                                    <div className="team-image">
-                                        <img className="img-responsive img-thumbnail bounce-in"
-                                             src="images/team/team02.jpg" alt=""/>
-                                        <div className="overlay">
-                                            <a className="preview btn btn-outlined btn-primary"
-                                               href="images/team/team02.jpg" rel="prettyPhoto"><i
-                                                className="fa fa-eye"></i></a>
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team02.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team02.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-md-3 col-xs-6">
-                                <div className="center team-member">
-                                    <div className="team-image">
-                                        <img className="img-responsive img-thumbnail bounce-in"
-                                             src="images/team/team03.jpg" alt=""/>
-                                        <div className="overlay">
-                                            <a className="preview btn btn-outlined btn-primary"
-                                               href="images/team/team03.jpg" rel="prettyPhoto"><i
-                                                className="fa fa-eye"></i></a>
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team03.jpg" alt=""/>
+                                            <div className="overlay">
+                                                <a className="preview btn btn-outlined btn-primary"
+                                                   href="images/team/team03.jpg" rel="prettyPhoto"><i
+                                                    className="fa fa-eye"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-md-3 col-xs-6">
-                                <div className="center team-member">
-                                    <div className="team-image">
-                                        <img className="img-responsive img-thumbnail bounce-in"
-                                             src="images/team/team04.jpg" alt=""/>
+                                <div className="col-md-3 col-xs-6">
+                                    <div className="center team-member">
+                                        <div className="team-image">
+                                            <img className="img-responsive img-thumbnail bounce-in"
+                                                 src="images/team/team04.jpg" alt=""/>
+                                        </div>
                                     </div>
                                 </div>
                             </div>

919e9150d2c168ff38d50cabf1fbba552c1804a8
m.ilieva <magdalena.e.ilieva@gmail.com>
2019-01-08 18:42:14 +0200

    responsive header - trying to be responsive in progress

--- client/src/components/lavenderOil/Responsive-Header.js
+++ client/src/components/lavenderOil/Responsive-Header.js
@@ -19,12 +19,46 @@ class ImageGrid extends Component {
             options: defaults
         };
         this.placeholder = React.createRef();
+        this.gridContainer = React.createRef();
+
+        // this.generateCells = this.generateCells.bind(this);
+
+        this.mouseLeaveListener = this.mouseLeaveListener.bind(this);
+        this.mouseLeaveListener = this.mouseLeaveListener.bind(this);
     }
 
 
-    componentDidMount() {
 
-        this.generateContent();
+    // updateDimensions = () => {
+    //     let {rows, columns, margin} = this.state.options;
+    //     // let {width, height} = this.state;
+    //
+    //     let currentWidth = (window.innerWidth / columns) - margin;
+    //     let currentHeight = (window.innerWidth / rows) - margin;
+    //
+    //     // if (width !== currentWidth || height !== currentHeight) {
+    //         this.setState({width: currentWidth, height: currentHeight});
+    //
+    //         this.generateCells(currentWidth, currentWidth);
+    //     // }
+    // }
+
+
+
+
+    componentDidMount() {
+        let {rows, columns, margin} = this.state.options;
+
+        this.placeholder.current.appendChild(this.gridContainer.current);
+
+        let width = (this.gridContainer.current.offsetWidth / columns) - margin;
+        let height = (this.gridContainer.current.offsetHeight / rows) - margin;
+        this.generateCells(width, height);
+        //
+        // this.placeholder.current.addEventListener("resize", this.generateCells(width, height));
+        this.placeholder.current.addEventListener("mouseover", e => this.mouseOverListener(e));
+
+        this.placeholder.current.addEventListener("mouseleave", e => this.mouseLeaveListener(e, width, height));
 
     }
 
@@ -49,19 +83,27 @@ class ImageGrid extends Component {
         TweenMax.to(tile, time, {css: {backgroundPosition: left + "px " + top + "px"}, ease: Power1.easeOut});
     }
 
-    generateContent = () => {
+    mouseOverListener = (e) => {
+        let allTiles = e.currentTarget.querySelectorAll(".gridTile");
+        for (let t = 0, le = allTiles.length; t < le; t++) {
+            TweenMax.to(allTiles[t], this.state.options.animTime, {
+                css: {backgroundPosition: "0px 0px"},
+                ease: Power1.easeOut
+            });
+        }
+    }
+
+    mouseLeaveListener = (e, width, height) => {
+        let {animTime} = this.state.options;
+        let allTiles = e.currentTarget.querySelectorAll(".gridTile");
+        for (let ti = 0, len = allTiles.length; ti < len; ti++) {
+            this.fixTilePosition(allTiles[ti], ti, animTime, width, height);
+        }
+    }
+
+    generateCells = (width, height) => {
         let {rows, columns, margin} = this.state.options;
-
-
-        // let placeholder = document.getElementsByClassName(defaults.containerName)[0];
-        let container = document.createElement('div');
-        container.className = "gridContainer";
-        this.placeholder.appendChild(container);
-
         let gridTile;
-
-        let w = (container.offsetWidth / columns) - margin;
-        let h = (container.offsetHeight / rows) - margin;
         let arr = [];
 
         for (let i = 0, l = rows * columns; i < l; i++) {
@@ -69,47 +111,30 @@ class ImageGrid extends Component {
             gridTile.className = "gridTile";
             gridTile.style.backgroundImage = "url(" + defaults.imgSrc + ")";
 
-
-            arr = [(w + margin) * (i % columns), (h + margin) * Math.floor(i / columns), ((w + margin) * (i % columns) + w - margin), (h + margin) * Math.floor(i / columns), ((w + margin) * (i % columns) + w - margin), ((h + margin) * Math.floor(i / columns) + h - margin), (w + margin) * (i % columns), ((h + margin) * Math.floor(i / columns) + h - margin)];
-
-            // console.log(i + " ====>>> " + arr + " ||||| " + i%c  + " |||||| " + i/c);
-
+            arr = [(width + margin) * (i % columns),
+                (height + margin) * Math.floor(i / columns),
+                ((width + margin) * (i % columns) + width - margin),
+                (height + margin) * Math.floor(i / columns),
+                ((width + margin) * (i % columns) + width - margin),
+                ((height + margin) * Math.floor(i / columns) + height - margin),
+                (width + margin) * (i % columns),
+                ((height + margin) * Math.floor(i / columns) + height - margin)];
 
             TweenMax.set(gridTile, {
                 webkitClipPath: 'polygon(' + arr[0] + 'px ' + arr[1] + 'px,' + arr[2] + 'px ' + arr[3] + 'px, ' + arr[4] + 'px ' + arr[5] + 'px, ' + arr[6] + 'px ' + arr[7] + 'px)',
                 clipPath: 'polygon(' + arr[0] + 'px ' + arr[1] + 'px,' + arr[2] + 'px ' + arr[3] + 'px, ' + arr[4] + 'px ' + arr[5] + 'px, ' + arr[6] + 'px ' + arr[7] + 'px)'
             });
 
-            container.appendChild(gridTile);
-
+            this.gridContainer.current.appendChild(gridTile);
             this.fixTilePosition(gridTile, i);
         }
-
-        this.placeholder.addEventListener("mouseover", function (e) {
-            let allTiles = e.currentTarget.querySelectorAll(".gridTile");
-            for (let t = 0, le = allTiles.length; t < le; t++) {
-                TweenMax.to(allTiles[t], defaults.animTime, {
-                    css: {backgroundPosition: "0px 0px"},
-                    ease: Power1.easeOut
-                });
-            }
-        })
-
-        this.placeholder.addEventListener("mouseleave", function (e) {
-            let allTiles = e.currentTarget.querySelectorAll(".gridTile");
-            for (let ti = 0, len = allTiles.length; ti < len; ti++) {
-                this.fixTilePosition(allTiles[ti], ti, defaults.animTime,w, h);
-            }
-        })
-    };
-
-
-
+    }
 
     render() {
         return (
-            <div className="placeholder" ref={this.placeholder}/>
-
+            <div className="placeholder" ref={this.placeholder}>
+                <div className="gridContainer" ref={this.gridContainer}/>
+            </div>
         );
     }
 }

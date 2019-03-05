8310151c75bbc5c0658cb2fb0c82e881e9e15182
moonlighttt <magdalena.e.ilieva@gmail.com>
2019-03-05 16:06:03 +0200

    contact page almost ready

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import { InstagramLogo, FacebookLogo, GoogleLogo } from '../../../src/assets/images/logos.js';
 
 class Contact extends Component {
 
     render() {
         return (
-            <div id="lavender lavender-slider" className="main">
+            <div id="lavender-slider" className="main">
             <section id="main-slider-contact" className="no-margin">
                     <img src="/map-bg.png" className="back-image-oil" role="presentation"/>  
                     <div className="txt-container">
@@ -17,25 +17,30 @@ class Contact extends Component {
                     </div>
                     </div>       
             </section>
-            <div className="light-prpl">
+            <div className="prpl-title border-hr">
                     <div className="container ">
                         <h2 className="main-title">Contact Us</h2>
+                        <hr />
                     </div>
                   </div>
             <div id="content-wrapper">
-            <section id="contact">
-                <div className="cont-contactBtn">
-                    <div className='cont-flip'>
-                        <div className='back'>                         
-                            <div className="container contacts">
-                                <div className="col-xs-12 col-sm-6">
-                                    Mail: mefme.wegber.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="front">
-                            <button onClick={() => this.toggleForm()} className="flip close">X</button>
-                            <form className="contact-form container" action="">
+            <section id="contact" className="container">
+            <div className="col-sm-6 col-xs-12">
+            <div className="share active">
+                <div className="fabs" src="/instagram-logo.svg"><InstagramLogo></InstagramLogo></div>
+                <div className="fabs"><FacebookLogo></FacebookLogo></div>
+                <div className="fabs"><GoogleLogo></GoogleLogo></div>
+                <div className="content">
+                 Company:  Emona 29
+                 <br/>
+                 Name:  Emil Iliev 
+                 <br/>
+                 gmail: emona.bulgaria@gmail.com
+                </div>
+            </div>
+            </div>
+                <div className="cont-contactBtn col-sm-6 col-xs-12">
+                <form className="contact-form" action="">
                                 <input className="gutter" type="text"
                                        placeholder="Name"/>
                                 <input type="text" placeholder="Company"/>
@@ -44,8 +49,6 @@ class Contact extends Component {
                                 <textarea name="" id="tx" placeholder="Leave a message"></textarea>
                                 <input type="submit" value="Send"/>
                             </form>
-                        </div>
-                    </div>
                 </div>
             </section>
             </div>

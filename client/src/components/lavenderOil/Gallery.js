c7ede19b1f940758444a5e131a6bf645fe0a3f72
magdalena.ilieva <magdalena.ilieva@accedia.com>
2019-04-11 18:35:33 +0300

    adding lightbox

--- client/src/components/lavenderOil/Gallery.js
+++ client/src/components/lavenderOil/Gallery.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 import AOS from "aos";
 
 
 const RESPONSIVE_CLASS = 'col-sm-6 col-xs-12 col-md-3';
 
-const ptotos = [
-    "url(/gallery_1.jpg)",
-    "url(/gallery_2.jpg)",
-    "url(/gallery_3.jpg)",
-    "url(/gallery_4.jpg)",
-    "url(/gallery_5.jpg)",
-    "url(/gallery_6.jpg)",
-    "url(/gallery_7.jpg)",
-    "url(/gallery_8.jpg)"
+const images = [
+    "/gallery_1.jpg",
+    "/gallery_2.jpg",
+    "/gallery_3.jpg",
+    "/gallery_4.jpg",
+    "/gallery_5.jpg",
+    "/gallery_6.jpg",
+    "/gallery_7.jpg",
+    "/gallery_8.jpg"
 ];
 
 
 class Gallery extends Component {
+
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          photoIndex: 0,
+          isOpen: false,
+        };
+      }
 
     componentDidMount() {
         AOS.init();
@@ -27,14 +38,14 @@ class Gallery extends Component {
     }  
 
     renderPhotos = () => {
-        return ptotos.map((url) =>
-            <div className={RESPONSIVE_CLASS + ' img-animation'}>
-                <div className="center team-member">
+        return images.map((url, i) =>
+            <div key="url" className={RESPONSIVE_CLASS + ' img-animation'}>
+                <div className="center team-member" onClick={() => this.setState({ isOpen: true , photoIndex: i})}>
                     <div className="team-image">
                         <div className="front card">
                             <span className="more-icon"><i class="fas fa-plus"></i></span>                                 
                             <div className="production-img"
-                                    style={{backgroundImage: url}} data-aos="flip-up"
+                                    style={{backgroundImage: `url(${url})`}} data-aos="flip-up"
                                     data-aos-delay="0" data-aos-duration="1000">
                                       <div className="img-transperant"></div>
                                       <div className="img-gradient"></div>
@@ -48,6 +59,7 @@ class Gallery extends Component {
     }
 
     render() {
+        const { photoIndex, isOpen } = this.state;
         return (
             <section className="gallery-section">
                 <div className="container">
@@ -58,7 +70,25 @@ class Gallery extends Component {
                     <div id="meet-the-team" className="row">
                     {this.renderPhotos()}                        
                     </div>
-                </div>              
+                </div>  
+                {isOpen && (
+                    <Lightbox
+                        mainSrc={images[photoIndex]}
+                        nextSrc={images[(photoIndex + 1) % images.length]}
+                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                        onCloseRequest={() => this.setState({ isOpen: false })}
+                        onMovePrevRequest={() =>
+                        this.setState({
+                            photoIndex: (photoIndex + images.length - 1) % images.length,
+                        })
+                        }
+                        onMoveNextRequest={() =>
+                        this.setState({
+                            photoIndex: (photoIndex + 1) % images.length,
+                        })
+                        }
+                    />
+                    )}            
             </section>
         );
     }
